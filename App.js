9e105a7539941bc203d7167b9af68055fbfed516
rita_tony <rita_tony@hotmail.com>
2021-12-04 14:59:55 +0800

    Changed Text Message to Good Afternoon

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Rita 1234</Text>
+      <Text>Good Afternoon Rita</Text>
       <StatusBar style="auto" />
     </View>
   );
